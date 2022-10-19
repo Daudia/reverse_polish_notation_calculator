a896318f45dea51e1cb4faa616ebf00656a85268
DaudiaD <matheo.bregier@mines-ales.org>
2022-10-19 16:40:07 +0200

    - Creation of Controller class

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+import java.beans.PropertyChangeEvent;
+
+public class Controller implements java.beans.PropertyChangeListener{
+    public Controller() {
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
