397dbe80bb1aaadfe60d90128d92e976ba0216e4
DaudiaD <matheo.bregier@mines-ales.org>
2022-10-21 14:42:21 +0200

    - Implemented more Controller interfaces

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,69 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.beans.PropertyChangeEvent;
 
-public class Controller implements java.beans.PropertyChangeListener{
+public class Controller implements java.beans.PropertyChangeListener, java.awt.event.ActionListener, java.awt.event.KeyListener, java.awt.event.WindowListener{
     public Controller() {
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
 
     }
 }
